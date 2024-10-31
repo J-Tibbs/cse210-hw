728473529a8f5d9f9490cf7fadba57375137bd3f
J-Tibbs <jtibbitts100@byui.edu>
2024-10-31 13:52:16 -0600

    Functions Assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favoriteNum = PromptUserNumber();
+        int sqrdNum = SquareNumber(favoriteNum);
+        DisplayResult(name, sqrdNum);
+    }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName(){
+        Console.Write("What is your name? ");
+        string usersName = Console.ReadLine();
+        return usersName;
+    }
+    static int PromptUserNumber(){
+        Console.Write("What is your favorite number? ");
+        int favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+    static int SquareNumber(int favNum){
+        favNum *= favNum;
+        return favNum;
+    }
+    static void DisplayResult(string name, int squared){
+        Console.WriteLine($"{name}, the square of your number is {squared}.");
     }
 }
